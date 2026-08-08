af8799aebe739e48201344df9d9239d9f5b19a04
lauren <lauren@example.com>
2026-08-08 17:05:08 -0400

    move function into namespace and add comments

--- src/split.cpp
+++ src/split.cpp
@@ -7,18 +7,22 @@ using std::string;
 using std::istringstream;
 using std::getline;
 
-vector<string> split(const string& str, char delimeter) {
-    vector<string> pieces;
+namespace split {
+    vector<string> split(const string& str, char delimeter) {
+        vector<string> pieces;
 
-    istringstream stream(str);
-    string piece;
-    while (getline(stream, piece, delimeter)) {
-        pieces.push_back(piece);
+        istringstream stream(str);
+        string piece;
+        while (getline(stream, piece, delimeter)) {
+            pieces.push_back(piece);
+        }
+        // Edge case where the last character in the string is the delimeter
+        bool trailing = !str.empty() && str.back() == delimeter;
+        if (trailing) {
+            // Add an empty string, representing the empty string to the right of the ending
+            // delimeter
+            pieces.push_back("");
+        }
+        return pieces;
     }
-    // Edge case where the last character in the string is the delimeter
-    bool trailing = !str.empty() && str.back() == delimeter;
-    if (trailing) {
-        pieces.push_back("");
-    }
-    return pieces;
 }
