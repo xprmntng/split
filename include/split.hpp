0f2fda28cf0633507ddf5cdff6472653dc31957a
lauren <lauren@example.com>
2026-08-08 17:04:32 -0400

    add template function that splits a string on a delimeter and converts it to a given integral or floating point type

--- include/split.hpp
+++ include/split.hpp
@@ -1,6 +1,28 @@
 #pragma once
 
+#include <concepts>
+#include <expected>
 #include <string>
 #include <vector>
 
-std::vector<std::string> split(const std::string& str, char delimeter);
+#include "convert.hpp"
+
+namespace split {
+    std::vector<std::string> split(const std::string& str, char delimeter);
+
+    template <typename T>
+    requires std::integral<T> || std::floating_point<T>
+    std::expected<std::vector<T>, std::string> split_into(const std::string& str, char delimeter) {
+        std::vector<std::string> pieces = split(str, delimeter);
+        std::vector<T> pieces_converted;
+        pieces_converted.reserve(pieces.size());
+        for (const auto& piece : pieces) {
+            const auto result = convert::from_string<T>(piece);
+            if (!result) {
+                return std::unexpected(result.error());
+            }
+            pieces_converted.push_back(*result);
+        }
+        return pieces_converted;
+    }
+}
