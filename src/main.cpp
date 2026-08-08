f2a42369640c455524ebd18fa6db45502a1cbc72
lauren <lauren@example.com>
2026-08-08 17:43:56 -0400

    add ability to increment parts of version string

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <iostream>
 #include <expected>
+#include <unordered_map>
 
 #include "convert.hpp"
 #include "split.hpp"
@@ -11,18 +12,28 @@
 using types::u32;
 using split::split_into;
 
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        std::cout << "Usage: " << argv[0] << " <version string>" << std::endl;
+int main(int argc, const char** argv) {
+    std::unordered_map<std::string, int> choices = {
+        {"--major", 0},
+        {"--minor", 1},
+        {"--patch", 2}
+    };
+    if (argc < 3 || !choices.contains(argv[1])) {
+        std::cout << "Usage: " << argv[0] << " [--major|--minor|--patch] <version string>" << std::endl;
         return 1;
     }
-    const std::string version_string = argv[1];
+    const int choice = choices[argv[1]];
+    const std::string version_string = argv[2];
     const auto result = split_into<u32>(version_string, '.');
     if (!result) {
         std::cout << result.error() << std::endl;
         return 1;
     }
-    const auto& pieces = *result;
+    auto pieces = *result;
+    pieces[choice] += 1;
+    for (int i = choice + 1; i < 3; i++) {
+        pieces[i] = 0;
+    }
     for (const auto& piece : pieces) {
         std::cout << piece << " ";
     }
